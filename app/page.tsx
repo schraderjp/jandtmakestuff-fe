d566d1f30cdb44f60c16b8c8a4370a7759f2aaff
schraderjp <schraderjp@gmail.com>
2024-01-23 14:53:27 -0500

    added sanity client

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <main></main>;
+import { getProducts } from "./utils/sanityConfig";
+
+export default async function Home() {
+  const products = await getProducts();
+  return (
+    <main>
+      {products.map((product) => (
+        <div key={product._id}>{product.name}</div>
+      ))}
+    </main>
+  );
 }
