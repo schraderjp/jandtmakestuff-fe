2a35935cf01bad91a4d2232cd2b7c201b0200df7
schraderjp <schraderjp@gmail.com>
2024-02-08 20:41:35 -0500

    added price to product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import AddToCart from './AddToCart';
-import { Card, CardContent, CardFooter, CardHeader } from './ui/card';
-import { Product } from '@/types/type';
-import { PortableText } from '@portabletext/react';
+import React from "react";
+import AddToCart from "./AddToCart";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { Product } from "@/types/type";
+import { PortableText } from "@portabletext/react";
+import Currency from "./atoms/Currency";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -11,6 +12,9 @@ const ProductCard = ({ product }: { product: Product }) => {
         <CardHeader>{product.name}</CardHeader>
         <CardContent>
           <PortableText value={product.description} />
+          <p className="text-lg pt-4">
+            <Currency number={product.price} currency="usd" />
+          </p>
         </CardContent>
         <CardFooter>
           <AddToCart product={product} />
